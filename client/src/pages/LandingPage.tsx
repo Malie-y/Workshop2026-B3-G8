073c6292f0584305ed0eb2e15dcfbf93ea666965
Lisa Bureau <bureau.lisa03@gmail.com>
2026-09-22 19:42:36 +0200

    Fix landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,11 +4,11 @@ import '../styles/LandingPage.css'
 
 function LandingPage() {
     return (
-        <section>
+        <section className='home'>
             <div className='brand'>
                 <img src={logoEsa} alt="Logo ESA" />
                 <p className='subtitle'>EUROPEAN SPACE AGENCY</p>
-                <p className='title'>DEEP HORIZON <span>IX</span></p>
+                <p className='title'>DEEP HORIZON <span className='blue-text'>IX</span></p>
             </div>
             <div className='button-group'>
                 <NavLink to="/botanical-garden">
